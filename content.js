3beb548d91f2964fdc5e4da93875ff7c4ae84366
SandeepShakya <sandeepshakya2015@gmail.com>
2025-04-26 15:59:30 +0530

    add the  new loaded vido function

--- content.js
+++ content.js
@@ -82,6 +82,28 @@
     }
   };
 
+  const newVideoLoaded = async () => {
+    const bookmarkBtnExists =
+      document.getElementsByClassName("bookmark-btn")[0];
+    currentVideoBookmarks = await fetchBookmarks();
+
+    if (!bookmarkBtnExists) {
+      const bookmarkBtn = document.createElement("img");
+      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+      bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+      bookmarkBtn.title = "Click to bookmark current timestamp";
+      bookmarkBtn.style.width = "40px";
+      bookmarkBtn.style.height = "40px";
+
+      youtubeLeftControls =
+        document.getElementsByClassName("ytp-left-controls")[0];
+      youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+      youtubeLeftControls.appendChild(bookmarkBtn);
+      bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+    }
+  };
+
   chrome.runtime.onMessage.addListener((obj, sender, response) => {
     const { type, value, videoId } = obj;
 
